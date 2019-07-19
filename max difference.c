0ec6567889fd6164bb992526a91645bb72db96f2
unknown <dishantdua8956@gmail.com>
2019-07-19 23:33:51 +0530

    second

---
+++ max difference.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+//max(j-1);
+//a[j]>a[i];
+struct student
+{
+  int rollno;
+  int index;
+  
+};
+int comparator(const *p,const *q)
+{
+    int l=*(int *)p;
+    int r=*(int *)q;
+    //return (((struct student*)p)->index,((struct student *)p)->index);
+    return l-r;
+      
+}
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    
+    struct student arr[n];
+    //to scan records
+    for(i=0;i<n;i++)
+    {
+         scanf("%d",&arr[i].rollno);
+        // scanf("%c",&c1);
+        arr[i].index=i;
+       
+        
+    }
+    //before sorting
+    for(i=0;i<n;i++)
+    {
+        printf("%d %d \n",arr[i].rollno,arr[i].index);
+    }
+    qsort((void *)arr,(sizeof(arr)/sizeof(arr[0])),sizeof(arr[0]),comparator);
+    //after sorting with rollno
+    printf("\n");
+    int arr1[n],min,max=0;
+    for(i=0;i<n;i++)
+    {
+        printf("%d %d \n",arr[i].rollno,arr[i].index);
+        arr1[i]=arr[i].index;
+        
+    }
+    min=arr1[0];
+    for(i=1;i<n;i++)
+    {
+        if(arr1[i]-min>max)
+        {
+            max=arr1[i]-min;
+        }
+        min=(arr1[i]<min?arr1[i]:min);
+    }
+    printf("maximum difference is %d",max);
+    
+    
+}
