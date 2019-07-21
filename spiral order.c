e4ecee6012da6ca302dfe1ff5be76fffd581951e
unknown <dishantdua8956@gmail.com>
2019-07-21 11:59:18 +0530

    spiral

---
+++ spiral order.c
@@ -0,0 +1,52 @@
+
+/*n*n sipral matrix */
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=0,b=n-1,c=n-1,d=0,i,cnt=0;
+    int **arr=(int **)malloc(n*n);
+    for(i=0;i<n;i++)
+    {
+        arr[i]=(int*)(malloc(n*sizeof(int)));
+    }
+    while(a<=c && d<=b)
+    {
+        
+        for(i=d;i<=b;i++)
+        {
+            arr[a][i]=cnt++;
+        }
+        a++;
+        for(i=a;i<=c;i++)
+        {
+            arr[i][b]=cnt++;
+        }
+        b--;
+        for(i=b;i>=d;i--)
+        {
+            arr[c][i]=cnt++;
+        }
+        c--;
+        for(int i=c;i>=a;i--)
+        {
+            arr[i][d]=cnt++;
+        }
+        d++;
+        
+    }
+    for(i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d   ",arr[i][j]);
+            
+        }
+        printf("\n");
+    }
+    
+    
+    
+}
